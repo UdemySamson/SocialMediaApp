8bdd193069f9e5068d7dca1b8ff44531dc71deba
Alexandre Samson <alexander_samson@icloud.com>
2025-12-15 09:06:23 +0100

    Registrierung

--- SocialMediaApp/RegisterView.swift
+++ SocialMediaApp/RegisterView.swift
@@ -10,7 +10,40 @@ import SwiftUI
 import FirebaseAuth
 
 struct RegisterView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var errorMessage = ""
+    @State private var showAlert = false
+    @State private var alertMessage = ""
     var body: some View {
-        Text("Register View")
+        VStack {
+            TextField("E-mail Adresse", text: $email)
+                .autocorrectionDisabled()
+                .autocapitalization(.none)
+                .padding()
+                .background(Color(.secondarySystemBackground))
+            
+            SecureField("Passwort", text: $password)
+                .autocorrectionDisabled()
+                .autocapitalization(.none)
+                .padding()
+                .background(Color(.secondarySystemBackground))
+            
+            Button(action: register) {
+                Text("Registrieren")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    //.background(content: Color.green)
+            }
+        }
+    }
+    private func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                errorMessage = error.localizedDescription
+            } else {
+                print("User Created")
+            }
+        }
     }
 }
